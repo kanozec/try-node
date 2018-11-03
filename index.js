361cd3cf94e9de9ba18dcc1062d2ae1b250d4f7d
kanozec <kanozec@gmail.com>
2018-11-03 16:17:21 +0800

    Add the first view

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require("express");
-const app = express();
+const path = require('path')
+const express = require("express")
+const app = express()
 const indexRouter = require("./routes/index.js")
 const userRouter = require("./routes/users")
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.use('/', indexRouter)
 app.use('/users', userRouter)

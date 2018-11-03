dd5e89a66024f65ac0d5b319dbf24a1de2c4412a
kanozec <kanozec@gmail.com>
2018-11-03 16:46:33 +0800

    Make a simple middleware

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const userRouter = require("./routes/users")
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(function (req, res, next) {
+    console.log('1')
+    next()
+})
+
+app.use(function (req, res, next) {
+    console.log('2')
+    next(new Error('haha'))
+})
+
+app.use(function (req, res, next) {
+    console.log('3')
+    res.status(200).end()
+})
+
+app.use(function (err, req, res, next) {
+    console.log(err.stack)
+    res.status(500).send('Something broke!')
+})
+
 app.use('/', indexRouter)
 app.use('/users', userRouter)
 

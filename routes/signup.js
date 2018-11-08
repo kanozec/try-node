0422998e7caf13e32cf930010ab535cf8b2cf48a
kanozec <kanozec@gmail.com>
2018-11-08 15:31:24 +0800

    Initialize signup router

---
+++ routes/signup.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const checkNotLogin = require('../middlewares/check').checkNotLogin
+
+router.get('/', checkNotLogin, function (req, res, next) {
+  res.send('SignUp page')
+})
+
+router.post('/', checkNotLogin, function (req, res, next) {
+  res.send('SignpUp')
+})
+
+module.exports = router

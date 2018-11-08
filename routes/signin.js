ad66bb3a0ba3628730351085f3ccbbe80728305b
kanozec <kanozec@gmail.com>
2018-11-08 15:02:08 +0800

    Initialize signing router

---
+++ routes/signin.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const checkNotLogin = require('../middlewares/check').checkNotLogin
+
+router.get('/', checkNotLogin, function (req, res, next) {
+  res.send('Login page')
+})
+
+router.post('/', checkNotLogin, function (req, res, next) {
+  res.send('Login')
+})
+
+module.exports = router
